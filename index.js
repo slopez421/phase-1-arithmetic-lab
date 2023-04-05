a153ff60941169b1183148d2757b884cdbf52593
Shania Lopez <slopez@fandm.edu>
2023-04-05 15:56:56 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+
+const random = Math.ceil((Math.random() + 1));
+
+let num3 = 32
+let num4 = 7
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
